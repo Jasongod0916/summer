b60d8d89f871a278bb758aafca190316f986d628
Jasongod0916 <jasonchiu0916@gmail.com>
2023-08-01 09:37:20 +0800

    demo_0731

---
+++ demo_0731/Image.jsx
@@ -0,0 +1,8 @@
+import i from "./0731.jpg"
+
+const Image = () => {
+    return <div>
+        <img src={i} class="img-fluid" alt="0731.jpg" width={'50%'}/>
+    </div>
+}
+export default Image;
---
+++ demo_0731/index.jsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar.jsx';
+import Image from './Image.jsx';
+import Mid from './Mid.jsx';
+
+const Home = ()=>{
+    return <div>
+        <Navbar/>
+        <Image/>
+        <Mid />
+    </div>
+}
+export default Home
